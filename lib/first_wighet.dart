645fe550e126757e3d1457fafc786768db1f4737
erfanSdg <erfan.sadigh80@gmail.com>
2025-01-25 17:57:55 +0330

    2

---
+++ lib/first_wighet.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class FirstWighet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 200,
+      color:Colors.red,
+    );
+  }
+}
